874f61bb752d1aae51656b00ac31b4149537f52c
Maryam Heydarian <maryamhdr76@gmail.com>
2023-05-29 16:30:56 +0330

    enable search engine to show the city name

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ let el = document.querySelector.bind(document),
   _elements = {
     currentDayOfWeek: el("#current-day-of-week"),
     currentTime: el("#current-time"),
+    searchBtn: el("#search-btn"),
+    searchInput: el("#search-input"),
+    cityName: el("#city-name")
   };
 
 let currentDate = new Date();
+
+_elements.searchBtn.addEventListener("click", searchCity);
 
 function formatDate(date) {
   let daysOfWeek = [
@@ -19,6 +24,11 @@ function formatDate(date) {
 
   _elements.currentDayOfWeek.innerHTML = `${daysOfWeek[date.getDay()]},`;
   _elements.currentTime.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  _elements.cityName.innerHTML = _elements.searchInput.value ? _elements.searchInput.value : "New York, NY, USA";
 }
 
 formatDate(currentDate);
