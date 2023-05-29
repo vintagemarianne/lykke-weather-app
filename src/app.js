01be82e18a98f3740d86b14dabe92c1a5cec7954
Maryam Heydarian <maryamhdr76@gmail.com>
2023-05-29 16:56:36 +0330

    add change unit feature

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 let el = document.querySelector.bind(document),
+  isCurrentTempUnitCelsius = true,
   _elements = {
     currentDayOfWeek: el("#current-day-of-week"),
     currentTime: el("#current-time"),
     searchBtn: el("#search-btn"),
     searchInput: el("#search-input"),
-    cityName: el("#city-name")
+    cityName: el("#city-name"),
+    tempUnitBtn: el("#temp-unit-btn"),
+    currentTemp: el("#current-temp")
   };
 
 let currentDate = new Date();
 
 _elements.searchBtn.addEventListener("click", searchCity);
+_elements.tempUnitBtn.addEventListener("click", changeUnit);
 
 function formatDate(date) {
   let daysOfWeek = [
@@ -29,6 +33,22 @@ function formatDate(date) {
 function searchCity(event) {
   event.preventDefault();
   _elements.cityName.innerHTML = _elements.searchInput.value ? _elements.searchInput.value : "New York, NY, USA";
+}
+
+function changeUnit(event) {
+  event.preventDefault();
+  let currentTemp = _elements.currentTemp.innerHTML;
+
+  if (isCurrentTempUnitCelsius) {
+    _elements.tempUnitBtn.innerHTML = "F";
+    currentTemp = currentTemp * 1.8 + 32;
+  } else {
+    _elements.tempUnitBtn.innerHTML = "C";
+    currentTemp = (currentTemp - 32) / 1.8;
+  }
+
+  _elements.currentTemp.innerHTML = currentTemp;
+  isCurrentTempUnitCelsius = !isCurrentTempUnitCelsius;
 }
 
 formatDate(currentDate);
