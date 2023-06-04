7325b914e1c34e39d3eb2c0c62ea420096b40523
Maryam Heydarian <maryamhdr76@gmail.com>
2023-06-04 23:59:49 +0330

    Get city data from open weather api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 let el = document.querySelector.bind(document),
   isCurrentTempUnitCelsius = true,
+  apiKey = "616b14cbd38253313b3b8852fa77335d",
+  currentTemp = 0,
+  cityName = null,
   _elements = {
     currentDayOfWeek: el("#current-day-of-week"),
     currentTime: el("#current-time"),
@@ -7,13 +10,18 @@ let el = document.querySelector.bind(document),
     searchInput: el("#search-input"),
     cityName: el("#city-name"),
     tempUnitBtn: el("#temp-unit-btn"),
-    currentTemp: el("#current-temp")
+    currentTemp: el("#current-temp"),
+    cityInfoContainer: el("#city-info-container"),
+    weatherDesc: el("#weather-desc"),
+    currentWeatherIcon: el("#current-weahter-icon"),
+    noCityCard: el("#no-city-card")
   };
 
 let currentDate = new Date();
 
 _elements.searchBtn.addEventListener("click", searchCity);
-_elements.tempUnitBtn.addEventListener("click", changeUnit);
+// _elements.tempUnitBtn.addEventListener("click", changeUnit);
+_elements.cityInfoContainer.style.display = "none";
 
 function formatDate(date) {
   let daysOfWeek = [
@@ -32,12 +40,37 @@ function formatDate(date) {
 
 function searchCity(event) {
   event.preventDefault();
-  _elements.cityName.innerHTML = _elements.searchInput.value ? _elements.searchInput.value : "New York, NY, USA";
+  cityName = _elements.searchInput.value;
+  if (cityName === null) {
+    _elements.cityInfoContainer.style.display = "none";
+    _elements.noCityCard.style.display = "block";
+    return;
+  }
+
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`)
+    .then(showCityInfo)
+    .catch(function (error) {
+      alert(error.response.data.message);
+      _elements.searchInput.value = null;
+      _elements.cityInfoContainer.style.display = "none";
+      _elements.noCityCard.style.display = "block";
+    });
+}
+
+function showCityInfo(response) {
+  _elements.cityInfoContainer.style.display = "block";
+  _elements.noCityCard.style.display = "none";
+  currentTemp = Math.round(response.data.main.temp);
+  _elements.currentTemp.innerHTML = currentTemp;
+  cityName = response.data.name;
+  _elements.cityName.innerHTML = `${cityName}, ${response.data.sys.country}`;
+  _elements.weatherDesc.innerHTML = response.data.weather[0].description;
+  _elements.currentWeatherIcon.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 }
 
 function changeUnit(event) {
   event.preventDefault();
-  let currentTemp = _elements.currentTemp.innerHTML;
+  currentTemp = _elements.currentTemp.innerHTML;
 
   if (isCurrentTempUnitCelsius) {
     _elements.tempUnitBtn.innerHTML = "F";
